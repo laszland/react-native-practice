ff11e4301dbf317fabec600c6a483b0396989657
Lacus <lacus@lacuss-mbp.home>
2020-04-17 22:29:36 +0200

    TODO App: init, add header component

--- myProject/App.js
+++ myProject/App.js
@@ -1,46 +1,31 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/Header'
 
 export default function App() {
-  const  [people, setPeople] = useState([
-    { name: 'Vaszja', id: 1 },
-    { name: 'Rozi', id: 2 },
-    { name: 'Pupucsek', id: 3 },
-    { name: 'Pintyoke', id: 4 },
-    { name: 'Hugyosjozsi', id: 5 },
-    { name: 'Topike', id: 6 },
-    { name: 'Makekosz', id: 7 }
+ 
+  const [todos, setTodos] = useState([
+    { text: 'buy milk', key: 1},
+    { text: 'eat good', key: 2},
+    { text: 'be awesome', key: 3},
   ])
-
-  const pressHandler = (id) => {
-    console.log(id);
-    setPeople((prevPeople) => {
-      return prevPeople.filter(person => person.id != id);
-    })
-  }
 
   return (
     <View style={styles.container}>
-      
-      <FlatList
-        //numColumns={2} => it creates a gridlike appearence 
-        keyExtractor={ item => item.id }
-        data={people}
-        renderItem={ ({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item} key={item.key}> {item.name} </Text>
-          </TouchableOpacity>
-        )}
-      />
-
-      {/* <ScrollView>
-        { people.map(item => (
-            <View>
-              <Text style={styles.item} key={item.key}> {item.name} </Text>
-            </View>
-          )) }
-      </ScrollView> */}
-
+      {/* HEADER */}
+      <Header/>
+      <View style={styles.content}>
+        {/* TODO FORM */}
+        <View style={styles.list}>
+          {/* ACTUAL FLATLIST */}
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -48,26 +33,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20
-    //alignItems: 'center',
-    //justifyContent: 'center',
+    backgroundColor: '#fff'
   },
-  buttonContainer: {
+  content: {
+    padding: 40
+  },
+  list: {
     marginTop: 20
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200
-  },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24
   }
 });
